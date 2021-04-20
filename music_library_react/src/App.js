efe7641ff34fd74ae4f6d12666c45bf118b12d84
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-20 09:09:58 -0800

    Created UpdateAndRemove component.

--- music_library_react/src/App.js
+++ music_library_react/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import './components/AddSong'
 import AddSong from './components/AddSong';
+import UpdateAndRemove from './components/UpdateAndRemove'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <h1>APP COMPONENT</h1>
       <div>
         <AddSong/>
+        <UpdateAndRemove/>
       </div>
     </div>
   );
