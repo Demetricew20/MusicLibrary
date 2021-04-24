0422c7ecfe309bc66e11b8fc199154eff09f4dde
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-24 12:30:46 -0800

    Added cover art for all songs.

--- music_library_react/src/Components/Track.jsx
+++ music_library_react/src/Components/Track.jsx
@@ -4,7 +4,7 @@ import regulate from '../Assets/Audio/Regulate.m4a';
 import myBabe from '../Assets/Audio/My_Babe.m4a';
 import sittinOnTheDock from '../Assets/Audio/Sittin_on_the_Dock.m4a';
 import elevators from '../Assets/Audio/elevators.m4a';
-import yourAllINeed from '../Assets/Audio/itWasAGoodDay.m4a';
+import yourAllINeed from '../Assets/Audio/yourAllINeed.m4a';
 import itWasAGoodDay from '../Assets/Audio/itWasAGoodDay.m4a';
 import homeSweetHome from '../Assets/Audio/homeSweetHome.m4a';
 import walkingOnADream from '../Assets/Audio/walkingOnADream.m4a';
@@ -14,35 +14,45 @@ import './Track.css';
 function Track(props) {
 
     let source;
+    let coverImage;
 
     switch (props.song.title) {
         
         case 'The Light':
-            source = theLight
+            source = theLight;
+            coverImage = "https://img.discogs.com/vQe13Jxkg7ZLGPitVnlLX6zBf3Q=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1078477-1539502479-3034.jpeg.jpg";
             break;
         case "(Sittin' On) the Dock of the Bay":
-            source = sittinOnTheDock
+            source = sittinOnTheDock;
+            coverImage = 'https://img.discogs.com/hEni01dDePXfzhwKsIR6EbZRq80=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-7334278-1548884715-1249.jpeg.jpg';
             break;
         case "Elevators":
-            source = elevators
+            source = elevators;
+            coverImage = 'https://img.discogs.com/5n9SO5gAfhtncxUpxCtdGjsPKho=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-8284050-1480354583-2186.jpeg.jpg';
             break;
         case "Home Sweet Home":
-            source = homeSweetHome
+            source = homeSweetHome;
+            coverImage = 'https://img.discogs.com/wvOtWg0zWKopbGfjBgYEHkBDB-Q=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-2252583-1431858685-8618.jpeg.jpg';
             break;
         case "My Babe":
-            source = myBabe
+            source = myBabe;
+            coverImage = 'https://i.scdn.co/image/ab67616d00001e0220f3de1822791b066e563a2d';
             break;
         case "I'll Be There for You/You're All I Need to Get By":
-            source = yourAllINeed
+            source = yourAllINeed;
+            coverImage = 'https://img.discogs.com/QvfERMWv5q0i3HlcJ_VbUmzsZt0=/fit-in/600x525/filters:strip_icc():format(webp):mode_rgb():quality(90)/discogs-images/R-2516325-1556812692-5413.jpeg.jpg';
             break;
         case "It Was a Good Day":
-            source = itWasAGoodDay
+            source = itWasAGoodDay;
+            coverImage = 'https://e.snmc.io/i/600/s/b5cb358306fe681428a3e36f15421314/7842677';
             break;
         case "Regulate":
-            source = regulate
+            source = regulate;
+            coverImage = "https://img.discogs.com/MN7d6knGZHPlnqBP5IqVT6d2ToI=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1739433-1401690539-1852.jpeg.jpg";
             break;
         case "Walking On a Dream":
-            source = walkingOnADream
+            source = walkingOnADream;
+            coverImage = 'https://upload.wikimedia.org/wikipedia/en/6/60/Empire_of_the_Sun_-_Walking_on_a_Dream_%28album%29.png';
             break;
     
         default:
@@ -54,6 +64,7 @@ function Track(props) {
         <div className='song'>
             <div className='cover'>
                 <h1 className='title'>{props.song.title}</h1>
+                <img src={coverImage} width={300} height={300}></img>
                 <h4 className='artist'>Artist - {props.song.artist}</h4>
                 <h4 className='album'>Album - {props.song.album}</h4>
                 <h4 className='releaseDate'>{props.song.releaseDate}</h4>
